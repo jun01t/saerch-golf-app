49fc0e8f16a4f0d130159e945e1959499f53fa8d
junichi <tmdjnch0901@gmail.com>
2022-08-21 13:20:29 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+  import Home from './components/Home.tsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+     <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
